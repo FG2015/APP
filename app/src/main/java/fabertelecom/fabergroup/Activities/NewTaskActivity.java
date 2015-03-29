fd2a3c68db4792e5a253c4a8a6897d6f49cc295a
Aitor <aitor.amh@gmail.com>
2015-03-29 14:03:59 +0200

    NewTaskActivity

--- app/src/main/java/fabertelecom/fabergroup/Activities/NewTaskActivity.java
+++ app/src/main/java/fabertelecom/fabergroup/Activities/NewTaskActivity.java
@@ -1,10 +1,12 @@
 package fabertelecom.fabergroup.Activities;
 
+import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -33,6 +35,21 @@ public class NewTaskActivity extends ActionBarActivity {
         setContentView(R.layout.activity_nueva_tarea);
         findViews();
 
+        cancelar_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NewTaskActivity.this,TasksActivity.class);
+                startActivity(i);
+            }
+        });
+
+        aceptar_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NewTaskActivity.this,TasksActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     private void findViews() {
